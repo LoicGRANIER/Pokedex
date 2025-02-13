2f04b591fe2d71d66b5ca2cd6031e3454cd0c961
LoicGRANIER <Granier.loic@Laposte.net>
2025-02-13 14:02:29 +0100

    premier repo

---
+++ pokemon.js
@@ -0,0 +1,75 @@
+const MAX_POKEMON = 649;
+const listWrapper = document.querySelector('.list-wrapper');
+const searchInput = document.querySelector('#Search-input');
+const numberFilter = document.querySelector('#number');
+const nameFilter = document.querySelector('#name');
+const notFoundMessage = document.querySelector('#not-found-message');
+
+let allPokemon = [];
+
+fetch(`https://pokeapi.co/api/v2/pokemon?limit=${MAX_POKEMON}`)
+    .then((response) => response.json())
+    .then((data) => {
+        allPokemon = data.results;
+        console.log(allPokemon)
+        displayPokemons(allPokemon)
+
+    });
+
+async function fetchPokemonDataBeforeRedirect(id) {
+    try {
+        const [pokemon, pokemonSpecies] = await Promise
+            .all([fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((res) => res.json()
+
+            ),
+            fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`).then((res) => res.json()
+
+            ),
+            ])
+        return true
+    } catch (error) {
+        console.error('failed fetch')
+    }
+
+}
+
+function displayPokemons(pokemon) {
+    listWrapper.innerHTML = "";
+    pokemon.forEach((pokemon) => {
+        const pokemonID = pokemon.url.split("/")[6];
+        const listItem = document.createElement("div");
+        listItem.className = "list-item";
+        listItem.innerHTML = `
+    <div class="number-wrap">
+    <p class="caption-fonts">#${pokemonID}</p>
+    </div>
+    <div class="img-wrap">
+    <img src="https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/other/dream-world/${pokemonID}.svg"</div>
+ <div class="name-wrap">
+    <p class="body3-fonts">${pokemon.name}</p>
+    </div>`;
+
+        listItem.addEventListener("click", async () => {
+            const success = await
+                fetchPokemonDataBeforeRedirect(pokemonID);
+            if (success) {
+                window.location.href = `./detail.html?id=${pokemonID}`;
+            }
+        })
+
+        listWrapper.appendChild(listItem)
+    })
+}
+
+searchInput.addEventListener("keyup", handleSearch)
+
+function handleSearch()   {
+ const searchTerm = searchInput.value.toLowerCase();
+ let filteredPokemon; 
+ 
+ if(numberFilter.checked)  {
+    filteredPokemon = allPokemon.filter = `${pokemonID}`
+ }
+}
+
+
